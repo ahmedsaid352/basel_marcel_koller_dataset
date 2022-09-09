2e056bf436558caeef8ed74c884562e94ca00170
unknown <ahmedsaid2000321@gmail.com>
2022-09-09 18:55:01 +0200

    collect data

---
+++ collect_data.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+
+with open('html.txt', 'r') as file:
+    html = file.read().replace('\n', '')
+
+soup = BeautifulSoup(html, 'html.parser')
+
+result=[]
+tactic =[]
+
+result_ele = soup.select('#yw1 > table > tbody > tr > td:nth-child(10)')
+tactic_ele = soup.select('#yw1 > table > tbody > tr > td.zentriert.hauptlink')
+
+for i in range(len(result_ele)):
+    result.append(result_ele[i].text)
+    tactic.append(tactic_ele[i].text)
+
+information = {
+    'result':result,
+    'tactic':tactic,
+}
+df = pd.DataFrame(information)
+df.to_excel('datasetinfo.xlsx',index=False)
